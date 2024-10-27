cc279ef7390a9e5b7c2d22faa5151be4680990e5
ArtaxJay <okjaceauthor@gmail.com>
2024-10-27 02:41:19 +0100

    Modify 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,8 +2,7 @@ export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
     if (
-      this.constructor !== Building &&
-      this.evacuationWarningMessage === undefined
+      this.constructor !== Building && this.evacuationWarningMessage === undefined
     ) {
       throw Error(
         'Class extending Building must override evacuationWarningMessage'
