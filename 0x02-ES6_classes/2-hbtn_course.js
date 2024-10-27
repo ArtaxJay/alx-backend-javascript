7d979ca579b97fd37f61dae36b7520ef46813f0a
ArtaxJay <okjaceauthor@gmail.com>
2024-10-27 01:35:38 +0100

    Add 2-hbtn_course.js

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,59 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name =
+      typeof name === 'string'
+        ? name
+        : (() => {
+            throw new TypeError('Name must be a string');
+          })();
+    this._length =
+      typeof length === 'number'
+        ? length
+        : (() => {
+            throw new TypeError('Length must be a number');
+          })();
+    this._students = Array.isArray(students)
+      ? students
+      : (() => {
+          throw new TypeError('Must be an array');
+        })();
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    this._name =
+      typeof name === 'string'
+        ? name
+        : (() => {
+            throw new TypeError('Name must be a string');
+          })();
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(paramSize) {
+    this._length =
+      typeof paramSize === 'number'
+        ? paramSize
+        : (() => {
+            throw new TypeError('Length must be a number');
+          })();
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    this._students = Array.isArray(students)
+      ? students
+      : (() => {
+          throw new TypeError('Must be an array');
+        })();
+  }
+}
