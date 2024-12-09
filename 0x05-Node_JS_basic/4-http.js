a1fed09a92c6888f25e456854070e43f2615b3f4
ArtaxJay <okjaceauthor@gmail.com>
2024-12-09 12:56:14 +0100

    Fix: rewrite 4-https.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,16 @@
 const http = require('http');
 
 // Create the HTTP server
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello ALX!');
+const app = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hello ALX!');
 });
 
 // Set the server to listen on port 1245
 const PORT = 1245;
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
 
 module.exports = app;
