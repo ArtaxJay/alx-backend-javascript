ddb1b962a6c47c2900905d459a19e8f27fba7133
ArtaxJay <okjaceauthor@gmail.com>
2025-01-25 12:14:37 +0100

    Fix: update 5-payment.test.js 2

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,33 +1,41 @@
 const sinon = require('sinon');
 const { sendPaymentRequestToAPI } = require('./5-payment');
-const Utils = require('./5-payment').Utils;
+const { Utils } = require('./5-payment');
 
 describe('sendPaymentRequestToAPI', () => {
   let consoleSpy;
 
   beforeEach(() => {
-    // Set up a spy for console.log
     consoleSpy = sinon.spy(console, 'log');
   });
 
   afterEach(() => {
-    // Restore the original console.log function
     consoleSpy.restore();
   });
 
-  it('should log the total as 120 when called with 100 and 20', () => {
+  it('should call Utils.calculateNumber and log the total as 120 when called with 100 and 20', () => {
+    const utilsSpy = sinon.spy(Utils, 'calculateNumber');
+
     sendPaymentRequestToAPI(100, 20);
 
-    // Verify console.log is called once with the correct string
+    sinon.assert.calledOnce(utilsSpy);
+    sinon.assert.calledWithExactly(utilsSpy, 'SUM', 100, 20);
     sinon.assert.calledOnce(consoleSpy);
     sinon.assert.calledWithExactly(consoleSpy, 'The total is: 120');
+
+    utilsSpy.restore();
   });
 
-  it('should log the total as 20 when called with 10 and 10', () => {
+  it('should call Utils.calculateNumber and log the total as 20 when called with 10 and 10', () => {
+    const utilsSpy = sinon.spy(Utils, 'calculateNumber');
+
     sendPaymentRequestToAPI(10, 10);
 
-    // Verify console.log is called once with the correct string
+    sinon.assert.calledOnce(utilsSpy);
+    sinon.assert.calledWithExactly(utilsSpy, 'SUM', 10, 10);
     sinon.assert.calledOnce(consoleSpy);
     sinon.assert.calledWithExactly(consoleSpy, 'The total is: 20');
+
+    utilsSpy.restore();
   });
 });
